7176453813fc81b4a25db12ac5d9af8ff493e2b3
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-01 15:46:40 +0700

    add class Stacks and add method Stacks

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -16,6 +16,15 @@ namespace StackUsingLinkedList
             next = n;
         }
     }
+    class Stacks
+    {
+        Node top;
+
+        public Stacks()
+        {
+            top = null;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
