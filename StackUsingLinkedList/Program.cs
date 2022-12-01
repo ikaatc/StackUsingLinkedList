e4425e436eac2d3cb1d1ac10eb78ff38031d0b41
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-01 16:03:29 +0700

    add method display in class Stacks

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -47,6 +47,21 @@ namespace StackUsingLinkedList
             Console.WriteLine("\nThe popped element is:" + top.info);
             top = top.next; //Make top point to the next node in sequence
         }
+        public void display()
+        {
+            Node tmp;
+            if (empty())   //If stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Traverse the list from begginning till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
     }
     internal class Program
     {
